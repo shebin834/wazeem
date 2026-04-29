1152992499287ea26407a75bc72281e10a8a9262
shebin k <shebinkallankunnan@gmail.com>
2026-04-29 11:39:38 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -253,7 +253,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/+NMted9BasygyMDFl">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/MALAYALIFREAKSALL">[ᴍᴀʟᴀʏᴀʟɪғʀᴇᴀᴋs]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/MalluTheaterLinkz">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/MalluTheaterLinkz">[ᴍᴀʟʟᴜᴛʜᴇᴀᴛᴇʀs]</a></b>"""
 
     
     MOVIE_UPDATE_NOTIFY_TXT = """
