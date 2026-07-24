c5c46c2115b2e2d775e8716b2ae0b13892288948
shebin k <shebinkallankunnan@gmail.com>
2026-07-24 20:14:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Wazeem_search')   # Session name for the bot
 API_ID = int(environ.get('API_ID', '32557254')) # API ID from my.telegram.org
 API_HASH = environ.get('API_HASH', '448bb61d4711ef33afff691ac1bb0931')  # API Hash from my.telegram.org
-BOT_TOKEN = environ.get('BOT_TOKEN', "5699367227:AAGzPCkbRhWXbPLe33jvwUgKwl6BHiGysGg")    # Bot token from @BotFather
+BOT_TOKEN = environ.get('BOT_TOKEN', "5699367227:AAFhvwf033zzKqnB101K81Rye3kc1pn-23U")    # Bot token from @BotFather
 
 # ============================
 # Bot Settings Configuration
